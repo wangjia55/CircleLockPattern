f718a11ba9c2668c91b0f2e5e4006bada6113d93
王佳 <wangjia@cvte.cn>
2015-03-27 14:52:50 +0800

    Init Projects

---
+++ app/src/main/java/com/jacob/circle/lock/LockPatternView.java
@@ -0,0 +1,27 @@
+package com.jacob.circle.lock;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Package : com.jacob.circle.lock
+ * Author : jacob
+ * Date : 15-3-27
+ * Description : 圆形锁
+ */
+public class LockPatternView extends View {
+    public LockPatternView(Context context) {
+        super(context);
+    }
+
+    public LockPatternView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LockPatternView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
